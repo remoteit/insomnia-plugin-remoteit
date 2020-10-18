3e859797f4527add26f1fc0a36153a77275110e1
Benoit Menendez <menendez@penngrove.com>
2020-10-18 14:35:31 -0700

    Support for credentials file

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Insomnia plugin for remote.it API authentication
 
-This is a plugin for [Insomnia](https://insomnia.rest/) that allows the signing of HTTP Requests for [remote.it](https://remote.it/) API access.
+This is a plugin for [Insomnia](https://insomnia.rest/) that allows the signing of HTTP Requests for [remote.it](https://remote.it/) API authentication.
 
 ##  Installation
 
@@ -8,4 +8,46 @@ Install the `insomnia-plugin-remoteit` plugin from Preferences -> Plugins.
 
 ##  How to use
 
-Add the `remote.it API Authentication` template tag as Bearer token.
+Add the `remote.it API authentication` template tag as `Bearer token`.
+
+The plugin will get your credentials from your _remote.it_ credentials file located at
+`~/.remoteit/credentials`:
+
+```ini
+R3_ACCESS_KEY_ID=PV7BWOOSA34XBPHDYFFO
+R3_SECRET_ACCESS_KEY=UZGhn/fqEYUPnhX6kZ3Z5uces3i0dq9YNw2+cOKh
+```
+
+Your credential file may contain multiple profiles:
+
+```ini
+[default]
+R3_ACCESS_KEY_ID=PV7BWOOSA34XBPHDYFFO
+R3_SECRET_ACCESS_KEY=UZGhn/fqEYUPnhX6kZ3Z5uces3i0dq9YNw2+cOKh
+[test]
+R3_ACCESS_KEY_ID=OYJKQK4O43MXYI7JHSUI
+R3_SECRET_ACCESS_KEY=F/WCo0MH3uuyj1rYnXHDP+DqSIXkwKOpn/C0MbWa
+```
+
+Profile names are case-insensitive and the `default` profile will be selected by 
+default. You can specify the profile for your environment using the `R3_PROFILE` 
+Insomnia environment variable.
+
+```json
+{
+  "R3_PROFILE": "test"
+}
+```
+
+You can also specify your remote.it _Access Key ID_ and _Secret Access key_ directly 
+in the Insomnia environment as:
+
+```json
+{
+  "R3_ACCESS_KEY_ID": "PV7BWOOSA34XBPHDYFFO",
+  "R3_SECRET_ACCESS_KEY": "UZGhn/fqEYUPnhX6kZ3Z5uces3i0dq9YNw2+cOKh"
+}
+```
+
+This is not recommended as you can accidentally export your environment with your 
+credentials.
--- plugin.js
+++ plugin.js
@@ -1,4 +1,20 @@
+const fs = require('fs')
+const ini = require('ini')
+const os = require('os')
+const path = require('path')
 const httpSignature = require('http-signature')
+
+const R3_ACCESS_KEY_ID = 'R3_ACCESS_KEY_ID'
+const R3_SECRET_ACCESS_KEY = 'R3_SECRET_ACCESS_KEY'
+const R3_PROFILE = 'R3_PROFILE'
+
+const CREDENTIALS_FILE = '.remoteit/credentials'
+const DEFAULT_PROFILE = 'default'
+
+const SIGNATURE_ALGORITHM = 'hmac-sha256'
+const SIGNED_HEADERS = '(request-target) host date content-type content-length'
+
+const APPLICATION_JSON = 'application/json'
 
 class RequestWrapper {
   constructor(request) {
@@ -27,7 +43,7 @@ class RequestWrapper {
       case 'host':
         return this.url.hostname
       case 'content-type':
-        return 'application/json'
+        return APPLICATION_JSON
       case 'content-length':
         const body = this.request.getBody()
 
@@ -42,11 +58,15 @@ class RequestWrapper {
   }
 }
 
+function getSection(object, name) {
+  return object[Object.keys(object).find(key => key.toUpperCase() === name.toUpperCase())]
+}
+
 module.exports.templateTags = [
   {
     name: 'remoteit',
-    displayName: 'remote.it API Authentication',
-    description: 'remote.it API Authentication',
+    displayName: 'remote.it API authentication',
+    description: 'remote.it API authentication',
     args: [],
     async run(context) {
     }
@@ -57,16 +77,37 @@ module.exports.requestHooks = [
   async (context) => {
     const request = context.request
 
-    const keyId = request.getEnvironmentVariable('R3_ACCESS_KEY_ID')
-    const key = Buffer.from(request.getEnvironmentVariable('R3_SECRET_ACCESS_KEY'), 'base64')
+    let keyId = request.getEnvironmentVariable(R3_ACCESS_KEY_ID)
+    let key = request.getEnvironmentVariable(R3_SECRET_ACCESS_KEY)
 
-    const options = {
-      key,
-      keyId,
-      algorithm: 'hmac-sha256',
-      headers: '(request-target) host date content-type content-length'.split(' ')
+    if (!keyId || !key) {
+      const config = path.resolve(os.homedir(), CREDENTIALS_FILE)
+
+      if (fs.existsSync(config)) {
+        let credentials = ini.parse(fs.readFileSync(config, 'utf-8'))
+
+        const profile = request.getEnvironmentVariable(R3_PROFILE)
+
+        if (profile) {
+          credentials = getSection(credentials, profile)
+
+          if (!credentials) throw new Error(`remote.it profile ${profile} not found`)
+        } else {
+          credentials = getSection(credentials, DEFAULT_PROFILE) || credentials
+        }
+
+        keyId = credentials[R3_ACCESS_KEY_ID]
+        key = credentials[R3_SECRET_ACCESS_KEY]
+      }
     }
 
-    httpSignature.sign(new RequestWrapper(request), options)
+    if (!keyId || !key) throw new Error('no remote.it credentials')
+
+    httpSignature.sign(new RequestWrapper(request), {
+      keyId,
+      key: Buffer.from(key, 'base64'),
+      algorithm: SIGNATURE_ALGORITHM,
+      headers: SIGNED_HEADERS.split(/\s+/)
+    })
   }
 ]
